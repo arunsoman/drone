1ba52064df50016986cde3949bf3ccac6381f72f
Arun Soman <aarunsoman@gmail.com>
2017-06-26 01:11:37 +0530

    Create acrobat.py

---
+++ acrobat.py
@@ -0,0 +1,26 @@
+import motorDynamics as md
+import mortor
+
+define main():
+  mass = 1#kg
+  gForce = mass*9.98
+  initaialForce = gForce
+  
+  motor1 = motor()
+  motor2 = motor()
+  motor3 = motor()
+  motor4 = motor() 
+  
+  
+  def start():
+    torquePerMoyor = initialForce/4;
+    motor1.setTorgue(torquePerMotor);
+    motor2.setTorque(torquePerMotor)
+    motor3.setTorque(torquePerMotor)
+    motor4.setTorque(torquePerMotor)
+    
+  def land():
+    return
+  
+ def howerAt(mrter):
+    return
