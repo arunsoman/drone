dd661f8cb2e8b961a07f1ca2a890ed663cd6e0a8
Arun Soman <aarunsoman@gmail.com>
2017-06-26 00:52:15 +0530

    Create motor.py

---
+++ motor.py
@@ -0,0 +1,15 @@
+def motor();
+   motorWeight=.56
+   requestedAV = 0
+   currentAV = 0
+   propeller
+   
+   def startMotor(propeller):
+    this.propeller = propellera
+    return
+    
+   def setAngularVelocity(value):
+    requestedAV = value
+    
+   def getCurrentValue():
+     return currentAV
