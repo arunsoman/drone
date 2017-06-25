cfb50f53db739afb8252c321440633e9b1fd09f9
Arun Soman <aarunsoman@gmail.com>
2017-06-26 00:21:22 +0530

    Create pie.py

---
+++ pie.py
@@ -0,0 +1,31 @@
+
+
+import numpy as np
+import time as tm
+
+def createPID(Kp, Ki, Kd):
+  startTime = int(round(time.time() * 1000))
+  err,prevErr =0
+  errs= [0]
+  def compute(currTime):
+    return Kp*err+ 
+    ki*sum((int)i for i in errs) + 
+    Kd*slope(currTime)
+    
+  def start(error):
+    err = error
+    startTime = int(round(time.time() * 1000))
+    errs.append(error)
+    return compute(startTime)
+    
+  def update(error):
+    prevErr = err
+    err = error
+    return compute(int(round(time.time() * 1000)))
+    
+  return {'start': start, 'update', update}
+  
+def test():
+  pid = createPID(1, -.08, -.23)
+  print (pid.start(10))
+  print(pid.update(5))
