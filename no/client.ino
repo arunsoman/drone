a4a3e48584106e32b890d10068bdd41bf60d4900
arun.soman <aarun.soman@gmail.com>
2017-06-26 17:58:13 +0530

    refactor

--- no/client.ino
+++ no/client.ino
@@ -1,14 +1,44 @@
-Helixcarrier c = new Helixcarrier();
-void setup()                    // run once, when the sketch starts
-{
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  
-  Serial.println("Hello world!");  // prints hello with ending line break 
+#include <pt.h>
 
+Sensor sensor = new Sensor();
+static struct pt pt1;
+
+static int commandReader(struct pt *pt) { 
+    PT_BEGIN(pt); 
+    while(1) {
+    	bool command =commandFromPi();
+    	if(command==true){
+    		processCommand();
+    	}           
+        PT_WAIT_UNTIL(pt, !command ); 
+        sendSensorDataToPI(); 
+    } 
+    PT_END(pt);
+}
+
+bool commandFromPi(){
+	return Serial.available()>0;
+}
+
+void sendSensorDataToPI(){
+	String stat = sensor.getData();
+	Serial.write(stat);
+}
+
+void processCommand(){
+	String command = Serial.readString();
+}
+
+Helixcarrier c = new Helixcarrier();
+void setup()
+{
+  Serial.begin(9600); 
+  PT_INIT(pt1);
 
 }
 
-void loop()                       // run over and over again
+void loop()
 {
-  c.setStep(40, 40, 40, 40);                                // do nothing!
+  commandReader(pt1);
+  c.setStep(40, 40, 40, 40);//this should happen in processCommand
 }
