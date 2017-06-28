367e81eae2dd002a93494cfaa0485181b45641e2
arun.muraleedharan <arun.muraleedharan@flytxt.com>
2017-06-28 20:21:57 +0530

    added ComplementaryFilter

--- gyro.py
+++ gyro.py
@@ -64,12 +64,49 @@ def extractData():
 
     return gyro_out, gyro_out_scale, acc_out, acc_out_scale
 
-while True:
-    gyro_out, gyro_out_scale, acc_out, acc_out_scale = extractData()
 
-    print ("gyro_out: ", gyro_out)
-    print ("acc_out: ", acc_out)
-    print ("gyro_out_scale: ", gyro_out_scale)
-    print ("acc_out_scale: ", acc_out_scale)
-    print ("getRotation", getRotation(gyro_out_scale,"X"),getRotation(gyro_out_scale,"Y"),getRotation(gyro_out_scale,"Z"))
-    time.sleep(2)
+ACCELEROMETER_SENSITIVITY = 8192.0
+GYROSCOPE_SENSITIVITY = 65.536
+dt = 0.01
+M_PI = 3.14159265359
+
+
+def ComplementaryFilter(accData, gyrData, pitch, roll):
+
+    pitchAcc = 0.0
+    rollAcc = 0.0
+    # Integrate the gyroscope data -> int(angularSpeed) = angle
+    pitch += (gyrData[0] / GYROSCOPE_SENSITIVITY) * dt  # Angle around the X-axis
+    roll -= (gyrData[1] / GYROSCOPE_SENSITIVITY) *  dt  # Angle around the Y-axis
+
+    # Compensate for drift with accelerometer data if !bullshit
+    # Sensitivity = -2 to 2 G at 16Bit -> 2G = 32768 && 0.5G = 8192
+    forceMagnitudeApprox = abs(accData[0]) + abs(accData[1]) + abs(accData[2])
+    if (forceMagnitudeApprox > 8192 and forceMagnitudeApprox < 32768):
+            # Turning around the X axis results in a vector on the Y-axis
+        pitchAcc = math.atan2(accData[1], accData[2]) * 180 / M_PI
+        pitch = pitch * 0.98 + pitchAcc * 0.02
+
+        # Turning around the Y axis results in a vector on the X-axis
+        rollAcc = math.atan2(accData[0], accData[2]) * 180 / M_PI
+        roll = roll * 0.98 + rollAcc * 0.02
+    return pitch, roll
+
+
+if __name__ == '__main__':
+    import sys
+    pitch = 0
+    roll = 0
+    while True:
+        gyro_out, gyro_out_scale, acc_out, acc_out_scale = extractData()
+        pitch, roll = ComplementaryFilter(acc_out, gyro_out, pitch, roll)
+        # sys.stderr.write("Acc: %s \t Gyr: %s                     \r" %(acc_out_scale, gyro_out_scale))
+        sys.stderr.write(
+            "pitch: %s \t roll: %s                     \r" % (pitch, roll))
+        sys.stderr.flush()
+        # print ("gyro_out: ", gyro_out)
+        # print ("acc_out: ", acc_out)
+        # print ("gyro_out_scale: ", gyro_out_scale)
+        # print ("acc_out_scale: ", acc_out_scale)
+        # print ("getRotation", getRotation(gyro_out_scale,"X"),getRotation(gyro_out_scale,"Y"),getRotation(gyro_out_scale,"Z"))
+        time.sleep(.1)
