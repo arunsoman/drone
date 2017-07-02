ce56ac46df5db6581b12ec27763dfcd7a52ffb3f
arun <aarunsoman@gmail.com>
2017-07-02 23:30:22 +0530

    refactored gpscode

--- pi/sensor/gps.py
+++ pi/sensor/gps.py
@@ -1,3 +1,4 @@
+import serial
 import RPi.GPIO as GPIO
 import os, time
 from decimal import *
@@ -6,50 +7,48 @@ delay = 1
 
 GPIO.setmode(GPIO.BOARD)
 
+class GPS(object):
+    def __init__(self):
+        self.port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
 
-def find(str, ch):
-    for i, ltr in enumerate(str):
-        if ltr == ch:
-            yield i
+    def __find(str, ch):
+        for i, ltr in enumerate(str):
+            if ltr == ch:
+                yield i
 
+    def getLatLong(self):
+        fd = ''
+        while fd.find('$GPRMC') > 0:
+            fd = ''
+            ck = 0
+            while ck  <= 50:
+                rcv = self.port.read(10)
+                fd = fd + rcv
+                ck = ck + 1
 
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
-cd = 1
-while cd & <= 50:
-    ck = 0
-    fd = ''
-    while ck & <    = 50:
-        rcv = port.read(10)
+        # print fd
+        if '$GPRMC' in fd:
+            ps = fd.find('$GPRMC')
+            dif = len(fd) - ps
+            # print dif
+            if dif > 50:
+                data = fd[ps:(ps + 50)]
+                print data
+                p = list(self.__find(data, ","))
+                lat = data[(p[2] + 1):p[3]]
+                lon = data[(p[4] + 1):p[5]]
 
-        fd = fd + rcv
-        ck = ck + 1
+                s1 = lat[2:len(lat)]
+                s1 = Decimal(s1)
+                s1 = s1 / 60
+                s11 = int(lat[0:2])
+                s1 = s11 + s1
 
-    # print fd
-    if '$GPRMC' in fd:
-        ps = fd.find('$GPRMC')
-        dif = len(fd) - ps
-        # print dif
-        if dif > 50:
-            data = fd[ps:(ps + 50)]
-            print data
-            p = list(find(data, ","))
-            lat = data[(p[2] + 1):p[3]]
-            lon = data[(p[4] + 1):p[5]]
+                s2 = lon[3:len(lon)]
+                s2 = Decimal(s2)
+                s2 = s2 / 60
+                s22 = int(lon[0:3])
+                s2 = s22 + s2
 
-            s1 = lat[2:len(lat)]
-            s1 = Decimal(s1)
-            s1 = s1 / 60
-            s11 = int(lat[0:2])
-            s1 = s11 + s1
-
-            s2 = lon[3:len(lon)]
-            s2 = Decimal(s2)
-            s2 = s2 / 60
-            s22 = int(lon[0:3])
-            s2 = s22 + s2
-
-            print s1
-            print s2
-
-    cd = cd + 1
-    print cd
+                print s1
+                print s2
